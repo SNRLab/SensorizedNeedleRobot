4beaf9616c2bfb2f6411011aada39b99b5f0c7bc
hannahwixom <86745791+hannahwixom@users.noreply.github.com>
2021-07-27 11:29:23 -0400

    Removed Virtual Function

--- stage_control/src/stage_control_node.cpp
+++ stage_control/src/stage_control_node.cpp
@@ -201,49 +201,6 @@ namespace stage_control
             this->hardware_z_publisher->publish(z_command);
         }
 
-        void MoveVirtualStage(double x, double z)
-        {
-        // Initialize JointTrajectoryPoint message
-            //std::vector<std::string> joint_names = {"joint1","joint2"};
-            //std::vector<trajectory_msgs::msg::JointTrajectoryPoint> points;
-
-            // Set x and z position
-            //auto x_position = Float64();
-            //auto z_position = Float64();
-
-            //x_position.data = x;
-            //z_position.data = z;
-            
-            //set x
-            //trajectory_msgs::msg::JointTrajectoryPoint point;
-            //point.time_from_start = rclcpp::Duration::from_seconds(0.0);  // start asap
-            //point.positions.resize(joint_names.size());
-
-            //point.positions[0] = 0.0;
-            //point.positions[1] = 0.0;
-
-            //set z
-            //trajectory_msgs::msg::JointTrajectoryPoint point2;
-            //point2.time_from_start = rclcpp::Duration::from_seconds(1.0);  // start asap
-            //point2.positions.resize(joint_names.size());
-
-            //point2.positions[0] = x;
-            //point2.positions[1] = z;
-
-            //points.push_back(point);
-            //points.push_back(point2);
-
-            // Create FollowJointTrajectory message
-            //control_msgs::action::FollowJointTrajectory_Goal goal_msg;
-            //goal_msg.goal_time_tolerance = rclcpp::Duration::from_seconds(5.0);
-            //goal_msg.trajectory.joint_names = joint_names;
-            //goal_msg.trajectory.points = points;
-
-            // Send to JointTrajectoryController server
-            //auto goal_handle_future = action_client->async_send_goal(goal_msg, opt);
-            
-        }
-
         double calculate_error(double x, double z)
         {
             return sqrt(pow(x - this->current_x, 2) + pow(z - this->current_z, 2));
